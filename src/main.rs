fd940ebee6875c53a4c60b120f2e64bb566ce70e
Alex Ripoll <alexripollpiera@gmail.com>
2024-07-25 20:19:09 +0200

    handle non existng commands

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,8 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    match input.trim_end().to_string() {
+        _ => eprintln!("{}: command not found", input.trim_end()),
+    }
 }
