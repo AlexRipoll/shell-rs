93b5ecb8460458011b706392b4b2a45beb5f9ce2
Alex Ripoll <alexripollpiera@gmail.com>
2024-07-26 11:48:25 +0200

    type builtin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 use std::process;
 
@@ -14,19 +13,50 @@ fn main() {
 
         let parsed_input: Vec<&str> = input.trim_end().split_whitespace().collect();
 
-        match parsed_input[0] {
-            "echo" => {
+        let command = match builtin(parsed_input[0]) {
+            Ok(x) => x,
+            Err(_) => {
+                eprintln!("{}: command not found", parsed_input[0].trim_end());
+                continue;
+            }
+        };
+
+        match command {
+            Builtin::Echo => {
                 let echo = parsed_input[1..].join(" ");
                 println!("{}", echo);
             }
-            "exit" => {
+            Builtin::Type => {
+                match builtin(parsed_input[1]) {
+                    Ok(_) => println!("{} is a shell builtin", parsed_input[1]),
+                    Err(_) => {
+                        eprintln!("{}: command not found", parsed_input[1].trim_end());
+                    }
+                };
+            }
+            Builtin::Exit => {
                 if let Some(status_code) = parsed_input.get(1) {
                     let status_code = status_code.parse::<i32>().expect("invalid status code");
                     process::exit(status_code);
                 }
                 process::exit(1);
             }
-            _ => eprintln!("{}: command not found", parsed_input[0].trim_end()),
         }
     }
+}
+
+fn builtin(input: &str) -> Result<Builtin, &str> {
+    match input {
+        "echo" => Ok(Builtin::Echo),
+        "type" => Ok(Builtin::Type),
+        "exit" => Ok(Builtin::Exit),
+        _ => Err("not a builtin"),
+    }
+}
+
+#[derive(Debug)]
+enum Builtin {
+    Echo,
+    Type,
+    Exit,
 }
