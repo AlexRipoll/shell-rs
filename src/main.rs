c5a48e5ab7b03c0c4cd92eef89608efaec6ec91a
Alex Ripoll <alexripollpiera@gmail.com>
2024-07-26 18:47:44 +0200

    run PATH binaries

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use core::panic;
-use std::fs::read_dir;
-use std::io::{self, Write};
+use std::env;
+use std::io::{self, stdout, Write};
 use std::path::Path;
-use std::{env, process};
+use std::process::Command;
+use std::process::{exit, Stdio};
 
 fn main() {
     loop {
@@ -14,53 +14,78 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        let parsed_input: Vec<&str> = input.trim_end().split_whitespace().collect();
+        let path = env::var("PATH").expect("PATH env not defined");
 
-        let command = match builtin(parsed_input[0]) {
+        let args: Vec<&str> = input.trim_end().split_whitespace().collect();
+        let cmd = args[0];
+
+        let builtin = match to_builtin(cmd) {
             Ok(x) => x,
             Err(_) => {
-                eprintln!("{}: command not found", parsed_input[0].trim_end());
+                // check if the first argument is a binary stored in one of the PATH directoires
+                let mut dirs = path.split(':');
+                if let Some(dir) = dirs.find(|&dir| {
+                    Path::new(format!("{}/{}", dir, cmd).as_str())
+                        .try_exists()
+                        .is_ok_and(|res| res)
+                }) {
+                    let cmd_path = format!("{}/{}", dir, cmd);
+
+                    let mut cmd_args = "";
+                    if args.len() > 1 {
+                        cmd_args = args[1];
+                    }
+
+                    let output = Command::new(cmd_path)
+                        .arg(cmd_args)
+                        .status()
+                        .expect("failed to execute process");
+
+                    println!("{}", output);
+                } else {
+                    eprintln!("{}: command not found", cmd.trim_end());
+                }
+
                 continue;
             }
         };
 
-        match command {
+        match builtin {
             Builtin::Echo => {
-                let echo = parsed_input[1..].join(" ");
+                let echo = args[1..].join(" ");
                 println!("{}", echo);
             }
             Builtin::Type => {
-                let cmd = parsed_input[1];
-                match builtin(cmd) {
-                    Ok(_) => println!("{} is a shell builtin", cmd),
+                let binary = args[1];
+                match to_builtin(binary) {
+                    Ok(_) => println!("{} is a shell builtin", binary),
                     Err(_) => {
-                        let path = env::var("PATH").expect("PATH env not defined");
                         let mut dirs = path.split(':');
                         if let Some(dir) = dirs.find(|&dir| {
-                            Path::new(format!("{}/{}", dir, cmd).as_str())
+                            Path::new(format!("{}/{}", dir, binary).as_str())
                                 .try_exists()
                                 .is_ok_and(|res| res)
                         }) {
-                            let path = format!("{}/{}", dir, cmd);
-                            println!("{} is {}", cmd, path);
+                            let path = format!("{}/{}", dir, binary);
+                            println!("{} is {}", binary, path);
                         } else {
-                            eprintln!("{}: not found", cmd.trim_end());
+                            eprintln!("{}: not found", binary.trim_end());
                         }
                     }
                 };
             }
             Builtin::Exit => {
-                if let Some(status_code) = parsed_input.get(1) {
+                if let Some(status_code) = args.get(1) {
                     let status_code = status_code.parse::<i32>().expect("invalid status code");
-                    process::exit(status_code);
+                    exit(status_code);
                 }
-                process::exit(1);
+                exit(1);
             }
         }
     }
 }
 
-fn builtin(input: &str) -> Result<Builtin, &str> {
+fn to_builtin(input: &str) -> Result<Builtin, &str> {
     match input {
         "echo" => Ok(Builtin::Echo),
         "type" => Ok(Builtin::Type),
